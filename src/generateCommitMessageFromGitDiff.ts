48424d490ddc326ab781d4c961f8f8c7e7f39704
Tom Woodley <thwoodley@gmail.com>
2023-04-02 12:27:04 +0100

    adjusted prompt message

--- src/generateCommitMessageFromGitDiff.ts
+++ src/generateCommitMessageFromGitDiff.ts
@@ -16,9 +16,9 @@ const INIT_MESSAGES_PROMPT: Array<ChatCompletionRequestMessage> = [
     role: ChatCompletionRequestMessageRoleEnum.System,
     // prettier-ignore
     content: `You are to act as the author of a commit message in git. Your mission is to create clean and comprehensive commit messages in the conventional commit convention and explain why a change was done. I'll send you an output of 'git diff --staged' command, and you convert it into a commit message.
-${config?.emoji? 'Use GitMoji convention to preface the commit.': 'Do not preface the commit with anything.'}
-${config?.description  ? 'Add a short description of WHY the changes are done after the commit message. Don\'t start it with "This commit", just describe the changes.': "Don't add any descriptions to the commit, only commit message."}
-Use the present tense. Lines must not be longer than 74 characters. Use ${translation.localLanguage} to answer.`
+'Use GitMoji convention to preface the commit.': 'Do not preface the commit with anything.
+'Add a short description of WHY the changes are done after the commit message. Don\'t start it with "This commit", just describe the changes.
+Use the present tense. Lines must not be longer than 74 characters. Use ${translation.localLanguage} to answer. You must use as few lines as possible. You must order lines starting with the most important. You do not need to mention the file names where changes have taken place`
   },
   {
     role: ChatCompletionRequestMessageRoleEnum.User,
