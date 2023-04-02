48a126a8df18598467dec04338030ec78d9fbdac
Tom Woodley <thwoodley@gmail.com>
2023-04-02 12:33:37 +0100

    📝 docs(generateCommitMessageFromGitDiff.ts): update INIT_MESSAGES_PROMPT to clarify commit message requirements

--- src/generateCommitMessageFromGitDiff.ts
+++ src/generateCommitMessageFromGitDiff.ts
@@ -16,7 +16,7 @@ const INIT_MESSAGES_PROMPT: Array<ChatCompletionRequestMessage> = [
     role: ChatCompletionRequestMessageRoleEnum.System,
     // prettier-ignore
     content: `You are to act as the author of a commit message in git. 
-    Your mission is to create clean and comprehensive commit messages in the conventional commit convention except that you shouldn't include (filename) and explain why a change was done. I'll send you an output of 'git diff --staged' command, and you convert it into a commit message.
+    Your mission is to create clean and comprehensive commit messages in the conventional commit  convention and explain why a change was done. I'll send you an output of 'git diff --staged' command, and you convert it into a commit message.
     Use GitMoji convention to preface the commit.
     Don\'t start it with "This commit", just describe the changes.
     Use the present tense. Lines must not be longer than 74 characters.
